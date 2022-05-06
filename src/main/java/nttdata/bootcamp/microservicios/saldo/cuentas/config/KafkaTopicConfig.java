de1131411a53af68a56e51ec34a68ed6e2781655
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2022-05-06 08:56:49 -0500

    update kafka

---
+++ src/main/java/nttdata/bootcamp/microservicios/saldo/cuentas/config/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package nttdata.bootcamp.microservicios.saldo.cuentas.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+//@Configuration
+public class KafkaTopicConfig {
+	/*
+	 * @Bean public NewTopic accountBalanceTopic() { return
+	 * TopicBuilder.name(" accountbalance").build(); }
+	 */
+}
